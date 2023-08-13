faa800f5dc5af17fbf266ce2b3ea776ff8aa780f
joq62 <jakim.leche@gmail.com>
2023-08-13 04:58:06 +0200

    created

---
+++ zigbee_spec_check.erl
@@ -0,0 +1,47 @@
+-module(zigbee_spec_check).
+
+-export([
+	 start/0,
+	 %% Support
+	 all_files/0,
+	 all_info/0
+	]).
+
+
+-define(Dir,".").
+-define(FileExt,".device").
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+start()->
+    
+    check(all_info()),
+    init:stop(),
+    ok.
+
+check([])->
+    io:format("Success, OK ! ~n");
+check([{ok,[{zigbee_device,Map}]}|T])->
+    io:format("Checking ~p~n",[Map]),
+    [<<"modelid">>,<<"module">>,<<"name">>,<<"state">>,<<"type">>]=lists:sort(maps:keys(Map)),
+    check(T).
+
+   
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+all_files()->
+    {ok,Files}=file:list_dir(?Dir),
+    FileNames=[filename:join(?Dir,Filename)||Filename<-Files,
+					     ?FileExt=:=filename:extension(Filename)],
+    FileNames.    
+all_info()->
+    [file:consult(File)||File<-all_files()].
+	    
+    
